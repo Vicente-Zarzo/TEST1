de315b1b7b876809cec21e969f01edf01d9d7909
Vicente-Zarzo <vizartou@teleco.upv.es>
2021-04-26 18:10:16 +0200

    Adicion de comentario SEGUNDO

--- app/src/main/java/es/upv/etsit/aatt/myapplication/MainActivity.java
+++ app/src/main/java/es/upv/etsit/aatt/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
         /* COMENTARIO */
+        /* OTRO COMENTARIO */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
